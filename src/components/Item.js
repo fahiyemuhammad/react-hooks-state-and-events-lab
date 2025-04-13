a663bfb1a5a0c7e3a8e02100e2a36a41fdce1779
Fahiye Muhammad <fahiye254@gmail.com>
2025-04-13 15:38:38 +0300

    Learning state and events in react

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [isInCart,setIsInCart] = useState (false);
+
+   function handleAdd(){
+      setIsInCart((prev)=> !prev)
+   }
+   
+  const itemClass = isInCart? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAdd}className="add">
+        {isInCart? "Remove from Cart":"Add to Cart"}
+        </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React , { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory,setSelectedCategory] = useState("All")
+
+  const filteredItems = selectedCategory === "All" 
+                         ? items
+                         : items.filter((item)=> item.category === selectedCategory) 
+  function handleFilter(event){
+    setSelectedCategory(event.target.value)
+  }
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleFilter}name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
+
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
